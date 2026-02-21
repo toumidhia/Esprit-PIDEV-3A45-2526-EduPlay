167b76d53f15765c122b8934ce1292d8a5379281
RachaKhm <racha.khammassi@esprit.tn>
2026-02-22 00:03:24 +0100

    admin event final part 1

--- migrations/Version20260221124709.php
+++ migrations/Version20260221124709.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20260216163652 extends AbstractMigration
+final class Version20260221124709 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,6 +20,8 @@ final class Version20260216163652 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE commande ADD stripe_payment_id VARCHAR(255) DEFAULT NULL, ADD is_paid TINYINT(1) NOT NULL');
+        $this->addSql('ALTER TABLE school_event CHANGE image_path image_path VARCHAR(255) DEFAULT NULL');
         $this->addSql('ALTER TABLE user DROP FOREIGN KEY FK_8D93D649727ACA70');
         $this->addSql('ALTER TABLE user CHANGE email email VARCHAR(255) DEFAULT NULL');
         $this->addSql('ALTER TABLE user ADD CONSTRAINT FK_8D93D649727ACA70 FOREIGN KEY (parent_id) REFERENCES user (id) ON DELETE CASCADE');
@@ -28,8 +30,10 @@ final class Version20260216163652 extends AbstractMigration
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE commande DROP stripe_payment_id, DROP is_paid');
+        $this->addSql('ALTER TABLE school_event CHANGE image_path image_path VARCHAR(255) NOT NULL');
         $this->addSql('ALTER TABLE user DROP FOREIGN KEY FK_8D93D649727ACA70');
         $this->addSql('ALTER TABLE user CHANGE email email VARCHAR(255) NOT NULL');
-        $this->addSql('ALTER TABLE user ADD CONSTRAINT FK_8D93D649727ACA70 FOREIGN KEY (parent_id) REFERENCES user (id) ON UPDATE NO ACTION ON DELETE NO ACTION');
+        $this->addSql('ALTER TABLE user ADD CONSTRAINT FK_8D93D649727ACA70 FOREIGN KEY (parent_id) REFERENCES user (id)');
     }
 }
